bc12032707ec3e1bdbf61c280455fa0cb4ffcecd
gangadhar <puramgangadhar006@gmail.com>
2024-04-20 12:19:29 +0530

    logger success commit

--- src/main/java/com/practice/docker/DockerApplication.java
+++ src/main/java/com/practice/docker/DockerApplication.java
@@ -1,13 +1,18 @@
 package com.practice.docker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DockerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DockerApplication.class, args);
-	}
+    private static final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class, args);
+		logger.info("logger for suuccess commit");
+    }
 
 }
