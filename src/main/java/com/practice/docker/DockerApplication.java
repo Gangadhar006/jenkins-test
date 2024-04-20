afc4530a39544f972f7e4868be6af2439c4f28ca
gangadhar <puramgangadhar006@gmail.com>
2024-04-20 12:53:13 +0530

    loger removed

--- src/main/java/com/practice/docker/DockerApplication.java
+++ src/main/java/com/practice/docker/DockerApplication.java
@@ -8,11 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DockerApplication {
 
-    private static final Logger logger = LoggerFactory.getLogger(DockerApplication.class);
-
     public static void main(String[] args) {
         SpringApplication.run(DockerApplication.class, args);
-		logger.info("logger for suuccess commit");
+
     }
 
 }
